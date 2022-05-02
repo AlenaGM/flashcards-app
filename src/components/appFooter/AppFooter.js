2d171d67887521d930ca33dfdbc731fb93708e1e
AlenaGM <alena.guillaume4@gmail.com>
2022-05-02 16:53:56 +0200

    upd styles

--- src/components/appFooter/AppFooter.js
+++ src/components/appFooter/AppFooter.js
@@ -3,8 +3,6 @@ import './appFooter.scss';
 const AppFooter = () => {
     return (
         <footer className="app__footer">
-            <div>Я - футер</div>
-            <div>Приятно Познакомиться</div>
         </footer>
     )
 }
