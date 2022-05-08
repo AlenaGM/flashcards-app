8091b75de39af325c8a5a47f052d1c9da15928e5
AlenaGM <alena.guillaume4@gmail.com>
2022-05-08 18:37:32 +0200

    impr

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -23,10 +23,10 @@ const Row = ({id, english, transcription, russian, tags, onDelete}) => {
     setEdit(!isEdit);
   }
 
-  const [state = {id:'', english:'', transcription:'', russian:'', tags:''}, setState] = useState();
+  const [state, setState] = useState({id:'', english:'', transcription:'', russian:'', tags:''});
 
-  const onValueChange = (e) => {
-    setState (!state, {
+  const handleChange = (e) => {
+    setState (state, {
       [e.target.name] : e.target.value
     })
   }
@@ -35,30 +35,30 @@ const Row = ({id, english, transcription, russian, tags, onDelete}) => {
 
       <tr className={classNames}>
         {isEdit ?
-        <>
-        <td><input className="input_edit" name="id" defaultValue={id} onChange={onValueChange}></input></td>
-        <td><input className="input_edit" name="english" defaultValue={english} onChange={onValueChange}></input></td>
-        <td><input className="input_edit" name="transcription" defaultValue={transcription} onChange={onValueChange}></input></td>
-        <td><input className="input_edit" name="russian" defaultValue={russian} onChange={onValueChange}></input></td>
-        <td><input className="input_edit" defaultValue={tags} onChange={onValueChange}></input></td>
-        <td>
-            <i className="fas fa-check icon icon__save" onClick = {handleSaveClick}> </i>
-            <i className="fas fa-ban icon icon__cancel" onClick = {handleCancelClick}></i>
-        </td>
-        </>
-        :
-        <>
-        <td>{id}</td>
-        <td>{english}</td>
-        <td>{transcription}</td>
-        <td>{russian}</td>
-        <td>{tags}</td>
-        <td>
-            <i className="fas fa-pen icon icon__edit" onClick = {handleEditClick}> </i>
-            <i className="fas fa-trash icon icon__delete" onClick = {onDelete}></i>
-        </td>
-        </>
-      }
+          <>
+          <td><input className="input_edit" name="id" defaultValue={id} onChange={handleChange}></input></td>
+          <td><input className="input_edit" name="english" defaultValue={english} onChange={handleChange}></input></td>
+          <td><input className="input_edit" name="transcription" defaultValue={transcription} onChange={handleChange}></input></td>
+          <td><input className="input_edit" name="russian" defaultValue={russian} onChange={handleChange}></input></td>
+          <td><input className="input_edit" defaultValue={tags} onChange={handleChange}></input></td>
+          <td>
+              <i className="fas fa-check icon icon__save" onClick = {handleSaveClick}> </i>
+              <i className="fas fa-ban icon icon__cancel" onClick = {handleCancelClick}></i>
+          </td>
+          </>
+          :
+          <>
+          <td>{id}</td>
+          <td>{english}</td>
+          <td>{transcription}</td>
+          <td>{russian}</td>
+          <td>{tags}</td>
+          <td>
+              <i className="fas fa-pen icon icon__edit" onClick = {handleEditClick}> </i>
+              <i className="fas fa-trash icon icon__delete" onClick = {onDelete}></i>
+          </td>
+          </>
+        }
       </tr>
     )
 }
