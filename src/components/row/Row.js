05661486862a6a381ed4a409264195e83b368ad4
AlenaGM <alena.guillaume4@gmail.com>
2022-05-17 14:02:39 +0200

    impr

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -46,29 +46,27 @@ class Row extends Component {
       <tr className={classNames}>
         {isEdit ?
           <>
-          <td><input type="text" className="input_edit" name="id" defaultValue={id} onChange={this.handleChange}></input></td>
-          <td><input type="text" className="input_edit" name="english" defaultValue={english} onChange={this.handleChange}></input></td>
-          <td><input type="text" className="input_edit" name="transcription" defaultValue={transcription} onChange={this.handleChange}></input></td>
-          <td><input type="text" className="input_edit" name="russian" defaultValue={russian} onChange={this.handleChange}></input></td>
-          <td><input type="text" className="input_edit" name="tags" defaultValue={tags} onChange={this.handleChange}></input></td>
-          <td>
-              <i className="fas fa-check icon icon__save" onClick={onSave}> </i>
-              <i className="fas fa-ban icon icon__cancel"
-              onClick = {this.onCancel}
-              ></i>
-          </td>
+            <td><input type="text" className="input_edit" name="id" defaultValue={id} onChange={this.handleChange}/></td>
+            <td><input type="text" className="input_edit" name="english" defaultValue={english} onChange={this.handleChange}/></td>
+            <td><input type="text" className="input_edit" name="transcription" defaultValue={transcription} onChange={this.handleChange}/></td>
+            <td><input type="text" className="input_edit" name="russian" defaultValue={russian} onChange={this.handleChange}/></td>
+            <td><input type="text" className="input_edit" name="tags" defaultValue={tags} onChange={this.handleChange}/></td>
+            <td>
+                <i className="fas fa-check icon icon__save" onClick={onSave}> </i>
+                <i className="fas fa-ban icon icon__cancel" onClick = {this.onCancel}></i>
+            </td>
           </>
           :
           <>
-          <td>{id}</td>
-          <td>{english}</td>
-          <td>{transcription}</td>
-          <td>{russian}</td>
-          <td>{tags}</td>
-          <td>
-              <i className="fas fa-pen icon icon__edit" onClick = {this.onEdit}> </i>
-              <i className="fas fa-trash icon icon__delete" onClick = {onDelete}></i>
-          </td>
+            <td>{id}</td>
+            <td>{english}</td>
+            <td>{transcription}</td>
+            <td>{russian}</td>
+            <td>{tags}</td>
+            <td>
+                <i className="fas fa-pen icon icon__edit" onClick = {this.onEdit}> </i>
+                <i className="fas fa-trash icon icon__delete" onClick = {onDelete}></i>
+            </td>
           </>
         }
       </tr>
