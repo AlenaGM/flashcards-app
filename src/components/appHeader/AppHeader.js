4793f507e822baf59b37d017f39c8625ebb6f5f2
AlenaGM <alena.guillaume4@gmail.com>
2022-05-23 15:16:55 +0200

    added logo

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,10 +1,12 @@
+import logo from '../../resources/img/card-img.svg';
 import './appHeader.scss';
 
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                    <span>English</span> Flashcards
+                <img className="logo" src={logo} alt="logo"></img>
+                <span>My</span> Flashcards
             </h1>
             <nav className="app__menu">
                 <ul>
