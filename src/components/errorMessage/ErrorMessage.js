2a3c69d14fbbeabdff37700b49c316a3212ee90d
AlenaGM <alena.guillaume4@gmail.com>
2022-05-24 15:23:25 +0200

    upd

--- src/components/errorMessage/ErrorMessage.js
+++ src/components/errorMessage/ErrorMessage.js
@@ -2,7 +2,7 @@ import img from './error.gif';
 
 const ErrorMessage = () => {
     return (
-        <img style={{ display: 'block', width: "450px",objectFit: 'contain', margin: "0 auto"}}  src={img} alt="Error"/>
+        <img style={{ display: 'block', maxWidth: "420px", objectFit: 'contain', margin: "0 auto"}}  src={img} alt="Error"/>
     )
 }
 
