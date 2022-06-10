d225c60d58811192b3a36b6a472f7bd76e5510db
AlenaGM <alena.guillaume4@gmail.com>
2022-06-10 13:13:41 +0200

    impr

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -2,53 +2,39 @@ import {useState} from "react";
 
 const Row = (props) => {
 
-  const [id, setId] = useState(props.id);
-  const [english, setEnglish] = useState(props.english);
-  const [transcription, setTranscription] = useState(props.transcription);
-  const [russian, setRussian] = useState(props.russian);
-  const [tags, setTags] = useState(props.tags);
+  const [state, setState] = useState(props);
+  //const [id, setId] = useState(props.id);
+  //const [english, setEnglish] = useState(props.english);
+  //const [transcription, setTranscription] = useState(props.transcription);
+  //const [russian, setRussian] = useState(props.russian);
+  //const [tags, setTags] = useState(props.tags);
   const [isEdit, setEdit] = useState(false);
 
   const onEdit = () => {
     setEdit(!isEdit);
   }
 
-  const handleIdChange = (e) => {
-      setId(e.target.value);
-  }
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.dataset.name]: e.target.value,
+    });
+  };
 
-  const handleEnglishChange = (e) => {
-    setEnglish(e.target.value);
-  }
-
-  const handleTranscriptionChange = (e) => {
-    setTranscription(e.target.value);
-  }
-
-  const handleRussianChange = (e) => {
-    setRussian(e.target.value);
-  }
-
-  const handleTagsChange = (e) => {
-    setTags(e.target.value);
-  }
-
-  const onSave = () => {
+  const onSave = (e) => {
     setEdit(!isEdit);
-    setId(id);
-    setEnglish(english);
-    setTranscription(transcription);
-    setRussian(russian);
-    setTags(tags)
+    setState({
+      ...state,
+      [e.target.dataset.name]: e.target.value,
+    });
+    console.log(`id: ${state.id}, english: ${state.english}, transcription: ${state.transcription}, russian: ${state.russian}, collection: ${state.tags}`);
   }
 
   const onCancel = () => {
     setEdit(!isEdit);
-    setId(props.id);
-    setEnglish(props.english);
-    setTranscription(props.transcription);
-    setRussian(props.russian);
-    setTags(props.tags)
+    setState({
+      ...props,
+    });
   }
 
   let classNames ='table__row';
@@ -61,11 +47,11 @@ const Row = (props) => {
     <tr className={classNames}>
       {isEdit ?
         <>
-          <td><input type="text" className="input_edit" name="setId" defaultValue={id} onChange={handleIdChange}/></td>
-          <td><input type="text" className="input_edit" name="setEnglish" defaultValue={english} onChange={handleEnglishChange}/></td>
-          <td><input type="text" className="input_edit" name="setTranscription" defaultValue={transcription} onChange={handleTranscriptionChange}/></td>
-          <td><input type="text" className="input_edit" name="setRussian" defaultValue={russian} onChange={handleRussianChange}/></td>
-          <td><input type="text" className="input_edit" name="setTtags" defaultValue={tags} onChange={handleTagsChange}/></td>
+          <td><input type="text" className="input_edit" data-name={"id"} defaultValue={state.id} onChange={handleChange}/></td>
+          <td><input type="text" className="input_edit" data-name={"english"} defaultValue={state.english} onChange={handleChange}/></td>
+          <td><input type="text" className="input_edit" data-name={"transcription"} defaultValue={state.transcription} onChange={handleChange}/></td>
+          <td><input type="text" className="input_edit" data-name={"russian"} defaultValue={state.russian} onChange={handleChange}/></td>
+          <td><input type="text" className="input_edit" data-name={"tags"} defaultValue={state.tags} onChange={handleChange}/></td>
           <td>
               <i className="fas fa-check icon icon__save" onClick={onSave}> </i>
               <i className="fas fa-ban icon icon__cancel" onClick = {onCancel}></i>
@@ -73,11 +59,11 @@ const Row = (props) => {
         </>
         :
         <>
-          <td>{id}</td>
-          <td>{english}</td>
-          <td>{transcription}</td>
-          <td>{russian}</td>
-          <td>{tags}</td>
+          <td>{state.id}</td>
+          <td>{state.english}</td>
+          <td>{state.transcription}</td>
+          <td>{state.russian}</td>
+          <td>{state.tags}</td>
           <td>
               <i className="fas fa-pen icon icon__edit" onClick = {onEdit}> </i>
               <i className="fas fa-trash icon icon__delete" onClick = {props.onDelete}></i>
