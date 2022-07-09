a7b13dd066dc6b39327fa70e02f3697f4185af94
AlenaGM <alena.guillaume4@gmail.com>
2022-07-09 15:50:58 +0200

    upd

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,46 +1,12 @@
-import { useState } from 'react';
-
-import Table from '../table/Table';
-import AddForm from '../addForm/AddForm';
-import library from '../../resources/data/words.json';
-
+import AppHome from '../appHome/AppHome';
 import '../App/App.scss';
 
 
 const HomePage = () => {
 
-    const [words, setWords] = useState(library);
-
-    const deleteItem = (id) => {
-        setWords(words.filter(word => word.id !== id));
-    }
-
-    const addItem = (english, transcription, russian, tags, id) => {
-
-        const newWord = {
-            english,
-            transcription,
-            russian,
-            tags,
-            id
-        }
-
-        const newArr = [...words, newWord];
-        setWords(newArr);
-    }
-
-    const saveItem = (id) => {
-        console.log(`save me! ${id}`)
-    }
-
-
     return (
         <div className="app__home">
-            <Table
-                words={words}
-                onDelete={deleteItem}
-                onSave={saveItem}/>
-            <AddForm onAdd={addItem}/>
+            <AppHome/>
         </div>
     )
 }
