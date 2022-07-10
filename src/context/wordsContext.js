481aa3265ef5fbccea0c9749ec056b859b021ffd
AlenaGM <alena.guillaume4@gmail.com>
2022-07-10 14:52:56 +0200

    cleared card

--- src/context/wordsContext.js
+++ src/context/wordsContext.js
@@ -1,4 +1,3 @@
-
 import { useState, useEffect, createContext } from 'react';
 
 const WordsContext = createContext();
@@ -6,7 +5,7 @@ const WordsContext = createContext();
 function WordsContextProvider(props) {
     const [words, setWords] = useState([]);
     const [loading, setLoading] = useState(true);
-    //const [error, setError] = useState(true);
+    const [error, setError] = useState(true);
 
     useEffect(() => {
       getWords();
@@ -17,17 +16,38 @@ function WordsContextProvider(props) {
       fetch('itgirlschool/api/words')
         .then((response) => response.json())
         .then((response) => {
-          console.log(response);
           setWords(response);
         })
-        .catch((errors) => console.log(errors))
+        .catch((errors) => setError(errors))
         .finally(() => {
           setLoading(false);
         });
     };
 
+    const editWords = (word) => {
+      fetch(`itgirlschool/api/words/${word.id}/update`, {
+        method: 'POST', // или 'PUT'
+        body: JSON.stringify(word), // данные могут быть 'строкой' или {объектом}!
+      })
+        .then(() => {
+          getWords();
+        })
+        .catch((errors) => setError(errors));
+    };
+
+    const deleteWords = (word) => {
+      fetch(`itgirlschool/api/words/${word.id}/delete`, {
+        method: 'POST', // или 'PUT'
+      })
+        .then(() => {
+          getWords();
+        })
+        .catch((errors) => setError(errors));
+    };
+
+
     return (
-    <WordsContext.Provider value={{words, loading}}>
+    <WordsContext.Provider value={{words, loading, error, editWords, deleteWords}}>
         {props.children}
     </WordsContext.Provider>
     );
