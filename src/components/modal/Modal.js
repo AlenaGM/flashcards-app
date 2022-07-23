323600c2e50f68b8fc6bd3098d8ef8d4c8f09a89
AlenaGM <alena.guillaume4@gmail.com>
2022-07-24 01:23:57 +0200

    upd

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -3,23 +3,33 @@ import { SelectContext } from '../../context/selectContext';
 
 import './modal.scss';
 
-const Modal = (props) => {
+const Modal = () => {
 
     const {modalActive, setModalActive} = useContext(SelectContext);
     const { setTerm } = useContext(SelectContext);
+    const { deleteOK, setDeleteOK } = useContext(SelectContext);
 
     const handleConfirm = () => {
+        setDeleteOK(true);
         setModalActive(false);
         setTerm('');
     }
 
+    const handleRefuse = () => {
+        setDeleteOK(false);
+        setModalActive(false);
+        setTerm('');
+    }
+
+    console.log (deleteOK);
+
     return (
-        <div className={modalActive? "modal active" : "modal"} onClick={()=>setModalActive(false)}>
+        <div className={modalActive? "modal active" : "modal"} onClick={()=>{setModalActive(false);setDeleteOK(false);}}>
             <div className={modalActive? "modal__content active" : "modal__content"}  onClick={e => e.stopPropagation()}>
             <div className="div">Удалить слово?</div>
                 <div className="div">
                     <button className='button button__size-small' onClick={handleConfirm}>Да</button>
-                    <button className='button button__size-small' onClick={()=>setModalActive(false)}>Нет</button>
+                    <button className='button button__size-small' onClick={handleRefuse}>Нет</button>
                 </div>
             </div>
         </div>
