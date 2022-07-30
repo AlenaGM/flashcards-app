77d6fd4017685d63399f23a761a50ffec6a11fc8
AlenaGM <alena.guillaume4@gmail.com>
2022-07-30 14:08:32 +0200

    upd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/itgirlschool', {
-      target: 'http://itgirlschool.justmakeit.ru',
+      target: 'https://itgirlschool.justmakeit.ru',
       changeOrigin: true,
       pathRewrite: {
         '/itgirlschool': '',
