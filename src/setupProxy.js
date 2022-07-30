8402bcad1d17156c9debb251397a6c604f15506c
AlenaGM <alena.guillaume4@gmail.com>
2022-07-30 14:12:16 +0200

    update proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/itgirlschool', {
-      target: 'https://itgirlschool.justmakeit.ru',
+      target: 'https://cors-everywhere.herokuapp.com/http://itgirlschool.justmakeit.ru',
       changeOrigin: true,
       pathRewrite: {
         '/itgirlschool': '',
