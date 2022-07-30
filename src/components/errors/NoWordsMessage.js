01ece38fdcddddffb042b830a55672b741e8b012
AlenaGM <alena.guillaume4@gmail.com>
2022-07-30 14:29:49 +0200

    test

--- src/components/errors/NoWordsMessage.js
+++ src/components/errors/NoWordsMessage.js
@@ -9,6 +9,7 @@ const NoWordsMessage = () => {
             <img className='error_img' src={img} alt="Error"/>
             <p>Sorry, we can't upload or download words!</p>
             <p>No connection! Or we are very busy!</p>
+            <p>test 14:30</p>
         </div>
     )
 }
