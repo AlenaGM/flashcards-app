7c44f3560fa092efd82c08e41a277e1b5dda6a0a
AlenaGM <alena.guillaume4@gmail.com>
2022-12-01 12:24:57 +0100

    update build for gh-pages

--- src/context/WordsContext.js
+++ src/context/WordsContext.js
@@ -1,84 +1,83 @@
-import { useState, useEffect, createContext } from 'react';
-
+import { useState, useEffect, createContext } from "react";
 
 export const WordsContext = createContext();
 
 function WordsContextProvider(props) {
+  const url = "http://itgirlschool.justmakeit.ru/api/words";
 
-  const url = 'https://cors-everywhere.herokuapp.com/http://itgirlschool.justmakeit.ru/api/words';
+  const [words, setWords] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errors, setError] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [term, setTerm] = useState("");
 
-    const [words, setWords] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [errors, setError] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [term, setTerm] = useState('');
+  useEffect(() => {
+    getWords();
+  }, []);
 
-    useEffect(() => {
-      getWords();
-    }, []);
-
-    const getWords = () => {
-      setLoading(true);
-      fetch(url)
-        .then((response) => response.json())
-        .then((response) => {
-          setWords(response);
-        })
-        .catch((errors) => setError(errors))
-        .finally(() => {
-          setLoading(false);
-        });
-    };
-
-    const editWords = (word) => {
-      fetch(`${url}/${word.id}/update`, {
-        method: 'POST',
-        body: JSON.stringify(word),
+  const getWords = () => {
+    setLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        setWords(response);
       })
-        .then(() => {
-          getWords();
-        })
-        .catch((errors) => setError(errors));
-    };
+      .catch((errors) => setError(errors))
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
-    const deleteWords = (id) => {
-      fetch(`${url}/${id}/delete`, {
-        method: 'POST',
+  const editWords = (word) => {
+    fetch(`${url}/${word.id}/update`, {
+      method: "POST",
+      body: JSON.stringify(word),
+    })
+      .then(() => {
+        getWords();
       })
-        .then(() => {
-          getWords();
-        })
-        .catch((errors) => setError(errors));
-    };
+      .catch((errors) => setError(errors));
+  };
 
-    const addWords = (word) => {
-      fetch(`${url}/add`, {
-        method: 'POST',
-        body: JSON.stringify(word),
+  const deleteWords = (id) => {
+    fetch(`${url}/${id}/delete`, {
+      method: "POST",
+    })
+      .then(() => {
+        getWords();
       })
-        .then(() => {
-          getWords();
-        })
-        .catch((errors) => setError(errors));
-    };
+      .catch((errors) => setError(errors));
+  };
 
-    return (
+  const addWords = (word) => {
+    fetch(`${url}/add`, {
+      method: "POST",
+      body: JSON.stringify(word),
+    })
+      .then(() => {
+        getWords();
+      })
+      .catch((errors) => setError(errors));
+  };
+
+  return (
     <WordsContext.Provider
-      value={{words,
-              loading,
-              errors,
-              currentPage,
-              term,
-              editWords,
-              deleteWords,
-              addWords,
-              setCurrentPage,
-              setTerm
-            }}>
-        {props.children}
+      value={{
+        words,
+        loading,
+        errors,
+        currentPage,
+        term,
+        editWords,
+        deleteWords,
+        addWords,
+        setCurrentPage,
+        setTerm,
+      }}
+    >
+      {props.children}
     </WordsContext.Provider>
-    );
+  );
 }
 
 export default WordsContextProvider;
-
